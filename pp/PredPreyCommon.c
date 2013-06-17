c0bf90b8cdb294418588dc4dd67bb7a5ca9c719c
Nuno Fachada <nfachada@laseeb.org>
2013-06-17 16:24:47 +0100

    Fix memory leak in PredPreyCommon.c
    File pointer was not being closed.

--- pp/PredPreyCommon.c
+++ pp/PredPreyCommon.c
@@ -22,6 +22,7 @@ int pp_load_params(PPParameters* parameters, const char* paramsFile, GError** er
 	unsigned int value;
 	unsigned int check = 0;
 	FILE * fp = fopen(paramsFile, "r");
+	int status = PP_SUCCESS;
 
 	if(fp == NULL) {
 		pp_error_create_return(err, PP_UNABLE_TO_OPEN_PARAMS_FILE, "Unable to open file \"%s\"", paramsFile);
@@ -34,93 +35,101 @@ int pp_load_params(PPParameters* parameters, const char* paramsFile, GError** er
 				parameters->init_sheep = value;
 				check = check | 1;
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "SHEEP_GAIN_FROM_FOOD") == 0) {
 			if ((1 & (check >> 1)) == 0) {
 				parameters->sheep_gain_from_food = value;
 				check = check | (1 << 1);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "SHEEP_REPRODUCE_THRESHOLD") == 0) {
 			if ((1 & (check >> 2)) == 0) {
 				parameters->sheep_reproduce_threshold = value;
 				check = check | (1 << 2);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "SHEEP_REPRODUCE_PROB") == 0) {
 			if ((1 & (check >> 3)) == 0) {
 				parameters->sheep_reproduce_prob = value;
 				check = check | (1 << 3);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "INIT_WOLVES") == 0) {
 			if ((1 & (check >> 4)) == 0) {
 				parameters->init_wolves = value;
 				check = check | (1 << 4);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "WOLVES_GAIN_FROM_FOOD") == 0) {
 			if ((1 & (check >> 5)) == 0) {
 				parameters->wolves_gain_from_food = value;
 				check = check | (1 << 5);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "WOLVES_REPRODUCE_THRESHOLD") == 0) {
 			if ((1 & (check >> 6)) == 0) {
 				parameters->wolves_reproduce_threshold = value;
 				check = check | (1 << 6);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "WOLVES_REPRODUCE_PROB") == 0) {
 			if ((1 & (check >> 7)) == 0) {
 				parameters->wolves_reproduce_prob = value;
 				check = check | (1 << 7);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "GRASS_RESTART") == 0) {
 			if ((1 & (check >> 8)) == 0) {
 				parameters->grass_restart = value;
 				check = check | (1 << 8);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "GRID_X") == 0) {
 			if ((1 & (check >> 9)) == 0) {
 				parameters->grid_x = value;
 				check = check | (1 << 9);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "GRID_Y") == 0) {
 			if ((1 & (check >> 10)) == 0) {
 				parameters->grid_y = value;
 				check = check | (1 << 10);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else if (strcmp(param, "ITERS") == 0) {
 			if ((1 & (check >> 11)) == 0) {
 				parameters->iters = value;
 				check = check | (1 << 11);
 			} else {
-				pp_error_create_return(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
+				pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, ERROR_MSG_REPEAT);
 			}	
 		} else {
-			pp_error_create_return(err, PP_INVALID_PARAMS_FILE, "Invalid parameter '%s' in parameters file", param);
+			pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, "Invalid parameter '%s' in parameters file", param);
 		}
 	}
 	if (check != 0x0fff) {
-			pp_error_create_return(err, PP_INVALID_PARAMS_FILE, "Insufficient parameters in parameters file (check=%d)", check);
+			pp_error_create_handle(err, PP_INVALID_PARAMS_FILE, "Insufficient parameters in parameters file (check=%d)", check);
 	}
-	return PP_SUCCESS;
+	
+	goto cleanup;
+	
+error_handler:
+	status = (*err)->code;
+	
+cleanup:
+	fclose(fp);
+	return status;
 } 
 
 /**
