a32736555b7a7d1a0e00d46b8fcb045496d0d854
Nuno Fachada <nfachada@laseeb.org>
2014-01-23 19:41:51 +0000

    GPU: Completely removed oiabitonic sort
    It didn't work and was way slower than sbitonic sort.

--- pp/pp_gpu_sort.c
+++ pp/pp_gpu_sort.c
@@ -5,7 +5,6 @@
 
 #include "pp_gpu_sort.h"
 #include "pp_gpu_sort_sbitonic.h"
-//~ #include "pp_gpu_sort_oiabitonic.h"
 
 /** Available sorting algorithms and respective properties. */
 PPGSortInfo sort_infos[] = {
@@ -13,10 +12,6 @@ PPGSortInfo sort_infos[] = {
 		ppg_sort_sbitonic_sort, 
 		ppg_sort_sbitonic_kernels_create, ppg_sort_sbitonic_kernelargs_set, ppg_sort_sbitonic_kernels_free, 
 		ppg_sort_sbitonic_events_create, ppg_sort_sbitonic_events_free, ppg_sort_sbitonic_events_profile}, 
-	//~ {"oia-bitonic", "PPG_SORT_OIABITONIC", 1, 
-		//~ ppg_sort_oiabitonic_sort, 
-		//~ ppg_sort_oiabitonic_kernels_create, ppg_sort_oiabitonic_kernelargs_set, ppg_sort_oiabitonic_kernels_free, 
-		//~ ppg_sort_oiabitonic_events_create, ppg_sort_oiabitonic_events_free, ppg_sort_oiabitonic_events_profile}, 
 	{NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL}
 };
 
