807d3385bf1db655245fbc8263ce24f5ed639686
Nuno Fachada <faken@fakenmc.com>
2014-12-17 20:43:54 +0000

    Update getlibs.sh to get Colt lib from proper location

--- java/lib/getlibs.sh
+++ java/lib/getlibs.sh
@@ -1,5 +1,1 @@
-wget http://acs.lbl.gov/software/colt/colt-download/releases/colt-1.2.0.zip
-unzip colt-1.2.0.zip colt/lib/colt.jar
-mv colt/lib/colt.jar colt.jar
-rm *.zip
-rm -rf colt/
+wget http://central.maven.org/maven2/colt/colt/1.2.0/colt-1.2.0.jar
