53236c9ec7de350c075bc54e2e583c190ac90391
Nuno Fachada <faken@fakenmc.com>
2015-02-02 17:47:29 +0000

    [JAVA] Use proper hash for deterministic agent sorting for parallel work implementations which require agent sorting, e.g., equal with repetition and equal row sync works.

--- java/src/org/laseeb/pphpc/AbstractAgent.java
+++ java/src/org/laseeb/pphpc/AbstractAgent.java
@@ -27,6 +27,9 @@
 
 package org.laseeb.pphpc;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 /**
@@ -114,21 +117,17 @@ public abstract class AbstractAgent implements IAgent {
 		
 		/* Get a unique profile for current agent based on its type and 
 		 * energy. */
-		int p1 = (this instanceof Wolf ? 1 << 31 : 1 << 30) | this.getEnergy();
+		int p1 = this.getEnergy() << 30 | this.getEnergy() << 2 | (this instanceof Wolf ? 0x1 : 0x2);
 		
 		/* Get a unique profile for the other agent based on its type and 
 		 * energy. */
-		int p2 = (otherAgent instanceof Wolf ? 1 << 31 : 1 << 30) | otherAgent.getEnergy();
+		int p2 = this.getEnergy() << 30 | this.getEnergy() << 2 | (this instanceof Wolf ? 0x1 : 0x2);
 
 		/* Get a hash for current agent. */
-		int h1 = p1;
-		h1 ^= (h1 >>> 20) ^ (h1 >>> 12);
-		h1 = h1 ^ (h1 >>> 7) ^ (h1 >>> 4);
+		int h1 = this.hash(p1);
 		
 		/* Get a hash for the other agent. */
-		int h2 = p2;
-		h2 ^= (h2 >>> 20) ^ (h2 >>> 12);
-		h2 = h2 ^ (h2 >>> 7) ^ (h2 >>> 4);
+		int h2 = this.hash(p2);
 		
 		/* Return comparison depending on hash, if hashes are equal, comparison
 		 * depends on agent type and energy (something we wish to minimize in
@@ -180,4 +179,48 @@ public abstract class AbstractAgent implements IAgent {
 		}
 	}
 
+	/**
+	 * Hash the agent value ID using Bob Jenkins hash.
+	 * @see <a href="http://burtleburtle.net/bob/hash/integer.html">http://burtleburtle.net/bob/hash/integer.html</a>
+	 * 
+	 * @param a Agent value ID to hash.
+	 * @return The hashed agent value ID.
+	 */
+	private int hash(int a) {
+		
+		a = (a+0x7ed55d16) + (a<<12);
+		a = (a^0xc761c23c) ^ (a>>19);
+		a = (a+0x165667b1) + (a<<5);
+		a = (a+0xd3a2646c) ^ (a<<9);
+		a = (a+0xfd7046c5) + (a<<3);
+		a = (a^0xb55a4f09) ^ (a>>16);
+		
+		return a;
+	}
+	
+//	private int hash(int a) {
+//
+//		MessageDigest digest = null;
+//		try {
+//			digest = MessageDigest.getInstance("SHA-256");
+//		} catch (NoSuchAlgorithmException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		digest.update(Integer.toString(a).getBytes());
+//		byte bytes[] = digest.digest();
+//		return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+//		
+//	}
+//	
+//	private int hash(int a) {
+//		
+//		a = (a ^ 61) ^ (a >> 16);
+//		a = a + (a << 3);
+//		a = a ^ (a >> 4);
+//		a = a * 0x27d4eb2d;
+//		a = a ^ (a >> 15);
+//		
+//		return a;
+//	}	
 }
