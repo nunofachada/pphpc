3aae4d58d4a801330465957eae773c6a203a9558
Nuno Fachada <nfachada@laseeb.org>
2015-11-02 14:06:46 +0000

    [OCL] Add license header to source files + remove configs

--- opencl/pp/pp_gpu_legacy.c
+++ opencl/pp/pp_gpu_legacy.c
@@ -1,3 +1,23 @@
+/*
+ * PPHPC-OCL, an OpenCL implementation of the PPHPC agent-based model
+ * Copyright (C) 2015 Nuno Fachada
+ *
+ * This file is part of PPHPC-OCL.
+ *
+ * PPHPC-OCL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PPHPC-OCL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PPHPC-OCL. If not, see <http://www.gnu.org/licenses/>.
+ * */
+
 /**
  * @file
  * Implementation of the legacy predator-prey OpenCL GPU simulation.
